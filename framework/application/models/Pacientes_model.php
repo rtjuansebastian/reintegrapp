f59ade8d57e66cc0859b6278df48113b2759d17e
Home <Home@Home-PC>
2017-04-16 17:10:38 -0500

    Buscar pacientes

--- framework/application/models/Pacientes_model.php
+++ framework/application/models/Pacientes_model.php
@@ -30,27 +30,51 @@ class Pacientes_model extends CI_Model
     }
     public function crear_paciente($data){
         $respuesta = array();
+        $validar_paciente = $this->pacientes_model->validar_cedula($data['cedula']);
+        if ($validar_paciente){
+            $respuesta['error'] = true;
+            $respuesta['mensaje'] = "La cédula ya se encuentra registrada.";
+            return $respuesta;
+        }
+        
         if ($this->db->insert('pacientes', $data)){
             $respuesta['error'] = false;
-            $respuesta['mensaje'] = "Usuario creado correctamente";
+            $respuesta['mensaje'] = "Paciente creado correctamente";
         }
         else{
             $respuesta['error'] = true;
-            $respuesta['mensaje'] = "Error al crear usuario";
+            $respuesta['mensaje'] = "Error al crear paciente";
         }
         return $respuesta;
         
     }
-    public function buscar_paciente($cedula){
-        $this->db->select('cedula, nombres, apellidos, generos.genero, estados_civil.estado, escolaridad.nivel, edad, tiempo_militancia, fecha_reintegracion');
-        $this->db->from('pacientes');
-        $this->db->join('generos', 'pacientes.genero = generos.id');
-        $this->db->join('estados_civil', 'pacientes.estado_civil = estados_civil.id');
-        $this->db->join('escolaridad', 'pacientes.escolaridad = escolaridad.id');        
+    public function validar_cedula($cedula)
+    {
+        $this->db->where('cedula', $cedula);
+        $query=  $this->db->get("pacientes");
+        if($query->num_rows()>0)
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }    
+    
+      public function buscar_paciente($cedula) {
+        $pacientes = array();
         $this->db->like('cedula', $cedula, 'both');
-        $query = $this->db->get();
-        return $query->row_array();
-    }   
+        $query = $this->db->get('pacientes');
+        foreach ($query->result() as $row) {
+            $pacientes[$row->cedula] = $row;
+        }
+        return $pacientes;
+    }
+   
     
-    
+
 }
+
+ 
+    
